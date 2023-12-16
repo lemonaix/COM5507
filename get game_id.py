30d0b4808dfd3327972a85a7eb0a13110c1b6b66
YAO Runzhe <116056920+lemonaix@users.noreply.github.com>
2023-12-17 01:24:39 +0800

    Update get game_id.py

--- get game_id.py
+++ get game_id.py
@@ -3,7 +3,7 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 # 指定HTML文件的路径
-file_path = 'C:\\Users\\A\\Desktop\\test.html'
+file_path = 'C:\\Users\\A\\Desktop\\steamdb source coede.html'
 
 # 直接使用文件句柄创建BeautifulSoup对象
 with open(file_path, 'r', encoding='utf-8') as file:
@@ -19,5 +19,5 @@ appid_values = [tag['data-appid'] for tag in appid_tags]
 appid_df = pd.DataFrame({'data-appid': appid_values})
 
 # 将DataFrame保存为Excel文件
-output_excel_path = 'C:\\Users\\A\\Desktop\\output_appid_data.xlsx'  # 您可以根据需要修改这个路径
+output_excel_path = 'C:\\Users\\A\\Desktop\\output_appid_data.xlsx'  
 appid_df.to_excel(output_excel_path, index=False)
